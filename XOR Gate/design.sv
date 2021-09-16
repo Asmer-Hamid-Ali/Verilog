1dd217851124d07e7371c305afdc459b20a4dce6
Asmer Hamid Ali <asmerali4feb@gmail.com>
2021-09-16 07:37:48 +0530

    Update design.sv

--- XOR Gate/design.sv
+++ XOR Gate/design.sv
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 
 module my_xor(output myout, input inA, input inB);
-  wire x, y, inA, inB, not_inA, not_inB;
+  wire x, y, not_inA, not_inB;
   and(x, inA, not_inB);
   and(y, inB, not_inA);
   not(not_inA, inA);
